8c99cbb2ec3d1d78364e3a3d63f4ed3bee112c8c
coddielam <edlam711@gmail.com>
2021-03-01 23:41:37 +0800

    feat: navbar expand roll out

--- frontend/src/components/nav/Nav.jsx
+++ frontend/src/components/nav/Nav.jsx
@@ -5,7 +5,14 @@ const Nav = () => {
     const handleOnClick = () => setCompassSpinned(!compassSpinned);
     return(
         <nav id="left-corner-nav-button" role="button" onClick={handleOnClick}>
-            <FontAwesomeIcon icon="compass" className={compassSpinned === '' ? '' : compassSpinned === true ? ' active' : 'inactive'}/>
+            <FontAwesomeIcon icon="compass" className={compassSpinned === '' ? '' : compassSpinned === true ? ' active zoomed' : 'inactive unzoomed'}/>
+
+            <ul id="nav-list" className={`${compassSpinned === '' ? '' : compassSpinned === true ? ' show' : ' unshow'}`}>
+                <li className="nav-item">Home</li>
+                <li className="nav-item">Projects</li>
+                <li className="nav-item">Resume</li>
+                <li className="nav-item">Background</li>
+            </ul>
         </nav>
     )
 }
